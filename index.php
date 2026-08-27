3f49412aaec60e14ea3dcc673018ecc66e4077a2
Veronee Maxius <veronee.maxius@convergesolve.com>
2026-08-27 16:30:19 -0400

    Create initial training website

--- index.php
+++ index.php
@@ -3,9 +3,82 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Hi ig</h1>
+
+
+    <div class="nav">
+         <ul class="navbar">
+                <!-- Making the different headers that will be on the menu bar-->
+                <li class="navbar_item">
+                    <a href="" class="navbar_links">
+                        GitHub
+                    </a>
+                </li>
+
+                <li class="navbar_item">
+                    <a href="" class="navbar_links">
+                        Visual Studio Code
+                    </a>
+                </li>
+
+                <li class="navbar_item">
+                    <a href="" class="navbar_links">
+                        Laragon
+                    </a>
+                </li>
+
+                <li class="navbar_item">
+                    <a href="" class="navbar_links">
+                        FAQs
+                    </a>
+                </li>
+
+            </ul>
+    </div>
+
+    <div class= "hero">
+        <h1>My Development Environment</h1>
+        <h6>27/08/2026</h6>
+    </div>
+    
+
+    <div class= "intro">
+        <p>My name is Veronee Maxius and this is my final task on my internship at Converge Solutions Inc. <br> <br>
+        On this website you will learn about: <br>
+        <ul><li>GitHub</li> <li>VS Code</li> <li>Laragon</li></ul>
+        </p>
+    </div>
+    
+
+    <div class= "github">
+        <h2>GitHub</h2>
+        <p>GitHub is a platform for building software. GitHub is a platform that lets you manage code, 
+            coordinate team updates, and explore shared projects to learn from real examples. 
+            GitHub brings together the tools and workflows you need across every stage of the 
+            software development life cycle
+        </p>
+
+    </div>    
+    <div class= "vscode">
+        <h2>Visual Studio Code</h2>
+        <p>Visual Studio Code is a code editor you can use to organize, manage, and write code. 
+            You can integrate other apps to add functionality, and it offers a wide range of 
+            customization built in. As the app is written in open-source code, you can really customize the 
+            program in any way you can imagine to fit your needs.
+        </p>
+    </div>
+
+    <div class= "laragon" id= "lar">
+        <h2>Laragon</h2>
+        <p>Laragon is a portable, isolated, fast and powerful universal development environment for PHP, 
+            Node.js, Python,etc. It is fast, lightweight, easy-to-use and easy-to-extend. Laragon is great 
+            for building and managing modern web applications. It is focused on performance, designed around
+             stability, simplicity, flexibility and freedom.
+        </p>
+    </div>
+    
 </body>
 </html>
